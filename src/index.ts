d82b4afd5394fc04e880031ed220c9894476a9a9
Dou Du <dou.du@icloud.com>
2024-05-26 16:07:41 +0200

    update changes

--- src/index.ts
+++ src/index.ts
@@ -51,7 +51,9 @@ const plugin: JupyterFrontEndPlugin<void> = {
         const content = new CounterWidget(browserFactory, theme);
         const widget = new MainAreaWidget<CounterWidget>({ content });
 
-        console.log('JupyterLab extension jupyterlab_mol_visualizer is activated!');
+        console.log(
+          'JupyterLab extension jupyterlab_mol_visualizer is activated!'
+        );
 
         // Watch for theme changes
         themeManager.themeChanged.connect((_, args) => {
